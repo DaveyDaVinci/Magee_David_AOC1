52fa905ea2e9ac97f2cfa0eb3a72a9f63d1ef888
DaveyDaVinci <DaveyDaVinci@gmail.com>
2012-10-30 15:05:24 -0400

    Test

---
+++ practiceAssignments/practice3/practice3Tests/practice3Tests.h
@@ -0,0 +1,13 @@
+//
+//  practice3Tests.h
+//  practice3Tests
+//
+//  Created by David Magee on 10/30/12.
+//  Copyright (c) 2012 David Magee. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface practice3Tests : SenTestCase
+
+@end
---
+++ practiceAssignments/practice3/practice3Tests/practice3Tests.m
@@ -0,0 +1,32 @@
+//
+//  practice3Tests.m
+//  practice3Tests
+//
+//  Created by David Magee on 10/30/12.
+//  Copyright (c) 2012 David Magee. All rights reserved.
+//
+
+#import "practice3Tests.h"
+
+@implementation practice3Tests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in practice3Tests");
+}
+
+@end
